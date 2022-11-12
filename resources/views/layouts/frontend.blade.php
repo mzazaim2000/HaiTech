d696b60058b8cc09916571bcff5cbf6a5bbb4921
Raziq Rakee <97151754+raziqrakee@users.noreply.github.com>
2022-11-13 01:34:46 +0800

    Created bootstrap5.css and bootstrap5.bundle.js

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -8,12 +8,18 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-
+        <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap5.css') }}">
+        <link rel="stylesheet" href="{{ asset('frontend/css/custom.css') }}">
     </head>
+    
     <body>
+
       <div>
         @yield('content')
       </div>
+
+      <script src=" {{ asset('frontend/js/jquery-3.6.1.min.js') }}"></script>
+      <script src=" {{ asset('frontend/js/bootstrap5.bundle.js') }}"></script>
+
     </body>
 </html>
