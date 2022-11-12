c5c64899641cd1d26114350a40ceed43f06680aa
Nelly <77133659+nells4@users.noreply.github.com>
2022-11-13 02:19:13 +0800

    Update signUpClient.blade.php

--- resources/views/pages/signUpClient.blade.php
+++ resources/views/pages/signUpClient.blade.php
@@ -1,53 +1,49 @@
+<!DOCTYPE html>
 
- 
- <body class="login">
-    <div>
-      <a class="hiddenanchor" id="signup"></a>
-      <a class="hiddenanchor" id="signin"></a>
- 
-      <div class="login_wrapper">
-        <div class="animate form login_form">
-          <section class="login_content">
-            <form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left" action="" method="post" name="myForm" autocomplete="off">
-              <h1>Admin Login</h1>
-              <div>
-                <input type="email" name="email" id="email" class="form-control" placeholder="Email" required="" />
-              </div>
-              <div>
-                <input type="password" name="password" id="password" class="form-control" placeholder="Password" required="" />
-              </div>
-              <div>
-                 <button type="submit" class="btn btn-primary">Cancel</button>
-                          <button id="send" type="submit" class="btn btn-success">Submit</button>
-              </div>
- 
-              <div class="clearfix"></div>
- 
-              <div class="separator">
-              
-              <div class="x_content bs-example-popovers">
- 
-          
-                </div>
- 
-                <div class="clearfix"></div>
-                <br />
- 
-                <div>
-                  
-                  <p>&nbsp;</p>
-                </div>
-              </div>
-            </form>
-          </section>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <!---<title> Responsive Registration Form | CodingLab </title>--->
+    <link rel="stylesheet" href="style.css">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+<body>
+  <div class="container">
+    <div class="title">Registration</div>
+    <div class="content">
+      <form action="#">
+        <div class="user-details">
+          <div class="input-box">
+            <span class="details">Full Name</span>
+            <input type="text" placeholder="Enter your name" required>
+          </div>
+          <div class="input-box">
+            <span class="details">Username</span>
+            <input type="text" placeholder="Enter your username" required>
+          </div>
+          <div class="input-box">
+            <span class="details">Email</span>
+            <input type="text" placeholder="Enter your email" required>
+          </div>
+          <div class="input-box">
+            <span class="details">Phone Number</span>
+            <input type="text" placeholder="Enter your number" required>
+          </div>
+          <div class="input-box">
+            <span class="details">Password</span>
+            <input type="text" placeholder="Enter your password" required>
+          </div>
+          <div class="input-box">
+            <span class="details">Confirm Password</span>
+            <input type="text" placeholder="Confirm your password" required>
+          </div>
         </div>
- 
-        <div id="register" class="animate form registration_form">
-          <section class="login_content">
-            
-          </section>
+        <div class="button">
+          <input type="submit" value="Register">
         </div>
-      </div>
+      </form>
     </div>
-  </body>
- 
+  </div>
+
+</body>
+</html>
