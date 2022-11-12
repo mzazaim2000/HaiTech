79c4967073760bc9909f373b8c526563a028c101
zuhair azaim <97045810+mzazaim2000@users.noreply.github.com>
2022-11-12 22:50:44 +0800

    Add files via upload

---
+++ login.blade.php
@@ -0,0 +1,52 @@
+
+ 
+ <body class="login">
+   <div>
+     <a class="hiddenanchor" id="signup"></a>
+     <a class="hiddenanchor" id="signin"></a>
+
+     <div class="login_wrapper">
+       <div class="animate form login_form">
+         <section class="login_content">
+           <form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left" action="" method="post" name="myForm" autocomplete="off">
+             <h1>Admin Login</h1>
+             <div>
+               <input type="email" name="email" id="email" class="form-control" placeholder="Email" required="" />
+             </div>
+             <div>
+               <input type="password" name="password" id="password" class="form-control" placeholder="Password" required="" />
+             </div>
+             <div>
+                <button type="submit" class="btn btn-primary">Cancel</button>
+                         <button id="send" type="submit" class="btn btn-success">Submit</button>
+             </div>
+
+             <div class="clearfix"></div>
+
+             <div class="separator">
+             
+             <div class="x_content bs-example-popovers">
+
+         
+               </div>
+
+               <div class="clearfix"></div>
+               <br />
+
+               <div>
+                 
+                 <p>&nbsp;</p>
+               </div>
+             </div>
+           </form>
+         </section>
+       </div>
+
+       <div id="register" class="animate form registration_form">
+         <section class="login_content">
+           
+         </section>
+       </div>
+     </div>
+   </div>
+ </body>
