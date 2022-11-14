4932536256fb97b5253697888d5e9e7de0081c9b
zuhair azaim <mz.azaim-2000@graduate.utm.my>
2022-11-14 17:12:16 +0800

    new edit

--- resources/views/pages/loginClient.blade.php
+++ resources/views/pages/loginClient.blade.php
@@ -28,7 +28,7 @@
                 <div class="form-content">
                     
                     <img src="images/logo.png" alt="" class="logo-img">
-                    <form action="#">
+                    <form action="index">
                         <div class="field input-field">
                             <input type="email" placeholder="Email" class="input">
                         </div>
