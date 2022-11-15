921a9bf71539e270e93480ce64b625e3099e5723
Nelly <77133659+nells4@users.noreply.github.com>
2022-11-15 21:08:58 +0800

    Update route

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -57,7 +57,7 @@
           User
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="#">Profile</a>
+          <a class="dropdown-item" href="updateprofileAdmin">Profile</a>
           <a class="dropdown-item" href="#">All services</a>
           <a class="dropdown-item" href="#">Logout</a>
         </div>
