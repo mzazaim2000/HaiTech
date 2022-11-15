94842b7c6d2e563cab02eaebcaaea97621372f06
Nelly <77133659+nells4@users.noreply.github.com>
2022-11-15 17:19:32 +0800

    Update frontend.php

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -1,19 +1,17 @@
 
 <!DOCTYPE html>
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>HaiTech</title>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title> HaiTech</title>
 
         <!-- Fonts -->
-        <meta name="description" content="">
         <meta name="viewpoint" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="{{ asset('frontend/css/loginclient.css') }}">
         <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap5.css') }}">
-        <link rel="stylesheet" href="{{ asset('frontend/css/custom.css') }}">
+        <link rel="stylesheet" href="{{ asset('frontend/css/frontend.css') }}">
+
 
     </head>
 
@@ -32,19 +30,18 @@
             </nav>
 
             <div class="dropdown">
-              <button class="dropbtn">User    </button>
+              <button class="dropbtn">User</button>
               <div class="dropdown-content">
                 <a href="#">Profile</a>
                 <a href="#">All services</a>
                 <a href="#">Logout</a>
               </div>
             </div>
-            <!--<a class="cta" href="#"><button>User</button></a>-->
          </header>
      
-      <div>
+      <!--<div>
         @yield('content')
-      </div>
+      </div>-->
 
       <script src=" {{ asset('frontend/js/jquery-3.6.1.min.js') }}"></script>
       <script src=" {{ asset('frontend/js/bootstrap5.bundle.js') }}"></script>
