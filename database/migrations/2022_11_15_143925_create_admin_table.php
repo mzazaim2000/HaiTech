96c26ed4807d445f0ece0fb4009ee0eda7bc6e8d
zuhair azaim <mz.azaim-2000@graduate.utm.my>
2022-11-15 23:07:52 +0800

    create db table

--- database/migrations/2022_11_15_143925_create_admin_table.php
+++ database/migrations/2022_11_15_143925_create_admin_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateAdminTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('user', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('firstname');
-            $table->string('surname');
+        Schema::create('admin', function (Blueprint $table) {
+            $table->id();
             $table->string('email')->unique();;
-            $table->string('contact');
             $table->string('password');
             $table->timestamps();
         });
@@ -31,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('admin');
     }
 }
