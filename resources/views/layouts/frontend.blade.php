f1f0c800b4b81f2986f84ede384a4c02d38f8511
Raziq Rakee <97151754+raziqrakee@users.noreply.github.com>
2022-11-15 22:36:55 +0800

    Update

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -16,10 +16,7 @@
     </head>
 
     <body>
-         <header>
-
-
-
+  <header>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">
