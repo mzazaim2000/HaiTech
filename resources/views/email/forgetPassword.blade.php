c4a26bca4e4e7d06f67dd09a61481a8291ca26ec
Nelly <77133659+nells4@users.noreply.github.com>
2022-11-26 18:06:21 +0800

    update forgetpassword.php

--- resources/views/email/forgetPassword.blade.php
+++ resources/views/email/forgetPassword.blade.php
@@ -1,19 +1,37 @@
-<body>
+<!--<body>
 <h1>Forget Password Email</h1>
    
 You can reset password from bellow link:
-<a href="{{ route('reset.password.get', $token) }}">Reset Password</a>
+<a href="{{ route('reset.password.get', $token) }}">Reset Password</a>-->
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <span class="preheader">Use this link to reset your password. The link is only valid for 24 hours.</span>
+    <!-- CSS -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap5.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/loginadmin.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/css/background.css') }}">   
+    <link rel="stylesheet" href="{{ asset('frontend/css/forgetpassword.css') }}">         
+    
+    <!-- Boxicons CSS -->
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+      <!-- End layout styles -->
+      <link rel="shortcut icon" href="/images/logo.png" />                   
+</head>
+
+<body>
+    <span class="preheader">Password reset link</span>
     <table class="email-wrapper" width="100%" cellpadding="0" cellspacing="0" role="presentation">
       <tr>
         <td align="center">
           <table class="email-content" width="100%" cellpadding="0" cellspacing="0" role="presentation">
             <tr>
               <td class="email-masthead">
-                <a href="https://example.com" class="f-fallback email-masthead_name">
-                [Product Name]
+                <a href="home" class="f-fallback email-masthead_name">
+                [Haibro]
               </a>
               </td>
             </tr>
@@ -25,52 +43,31 @@ You can reset password from bellow link:
                   <tr>
                     <td class="content-cell">
                       <div class="f-fallback">
-                        <h1>Hi</h1>
-                        <p>You recently requested to reset your password for your [Product Name] account. Use the button below to reset it. <strong>This password reset is only valid for the next 24 hours.</strong></p>
+                        <h1>Hello!</h1>
+                        <p>You are receiving this email bacause we received a password reset request from your account.</p>
                         <!-- Action -->
                         <table class="body-action" align="center" width="100%" cellpadding="0" cellspacing="0" role="presentation">
                           <tr>
                             <td align="center">
                               <!-- Border based button
-           https://litmus.com/blog/a-guide-to-bulletproof-buttons-in-email-design -->
+                                https://litmus.com/blog/a-guide-to-bulletproof-buttons-in-email-design -->
                               <table width="100%" border="0" cellspacing="0" cellpadding="0" role="presentation">
                                 <tr>
                                   <td align="center">
-                                    <a href="#" class="f-fallback button button--green" target="_blank">Reset your password</a>
+                                    <a href="{{ route('reset.password.get', $token) }}">Reset Password</a>
                                   </td>
                                 </tr>
                               </table>
                             </td>
                           </tr>
                         </table>
+                        <p>This password reset link will expire in 60 minutes.</p>
+                        <p>If you did not request a password reset, no further action is required.</p>
                         {{-- <p>For security, this request was received from a {{operating_system}} device using {{browser_name}}. If you did not request a password reset, please ignore this email or <a href="{{support_url}}">contact support</a> if you have questions.</p> --}}
-                        <p>Thanks,
-                          <br>The [Product Name] team</p>
+                        <p>Regards,
+                          <br>The Haibro team</p>
                         <!-- Sub copy -->
-                        <table class="body-sub" role="presentation">
-                          <tr>
-                            <td>
-                              <p class="f-fallback sub">If you’re having trouble with the button above, copy and paste the URL below into your web browser.</p>
-                              <p class="f-fallback sub">#</p>
-                            </td>
-                          </tr>
-                        </table>
                       </div>
-                    </td>
-                  </tr>
-                </table>
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <table class="email-footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
-                  <tr>
-                    <td class="content-cell" align="center">
-                      <p class="f-fallback sub align-center">
-                        [Company Name, LLC]
-                        <br>1234 Street Rd.
-                        <br>Suite 1234
-                      </p>
                     </td>
                   </tr>
                 </table>
