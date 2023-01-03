49d11807e3b00c1dd1ac3b8c17e07bff2442806a
zuhair azaim <mz.azaim-2000@graduate.utm.my>
2023-01-03 17:56:45 +0800

    edit invoice

--- resources/views/pages/adminInvoice.blade.php
+++ resources/views/pages/adminInvoice.blade.php
@@ -120,19 +120,8 @@
 
             <div class="container">
                  <div class="d-flex bd-highlight mb-3">
-                  <div class="me-auto p-2 bd-highlight"><h2>Invoice</h2></div>
-                  <div class="container-scroller">
-                    <form class="search-form d-none d-md-block" action="#">
-                      {{-- <i class="icon-magnifier"></i> --}}
-                      <input type="search" class="form-control" placeholder="Search Here" title="Search here">
-                    </form>
-                  </div>
-                  {{-- <div class="search__container">
-                    <p class="search__title"></p>
-                    <input class="search__input" type="text" placeholder="Search">
-                    <i class="icon-magnifier"></i>
-                </div> --}}
-                 </div>
+            <div class="me-auto p-2 bd-highlight"><h2>Invoice</div>
+            </div>
       
       @if(Session::has('order_message'))
       <div class="alert alert-success" role="alert">{{Session::get('order_message')}}</div>
@@ -149,7 +138,7 @@
             <th>Services</th>
             <th>Date</th>
             <th>Amount</th>
-            <th>Payment Status</th>
+            <th>Status</th>
             <th colspan="3" class="text-center">Action</th>
           </tr>
         </thead>
@@ -165,12 +154,12 @@
               <td>{{$service->company}}</td>
               <td>{{$service->services}}</td>
               <td>{{$service->date}}</td>
-              <td>{{$service->amount}}</td>
+              <td>{{$service->amount}}</td>php
               <td>{{$service->status}}</td>
                <td class="text-center">
-                <a href="invoiceForm" class="edit" onclick="editForm({{$service->id}})" data-toggle="modal"><i class="fa fa-money" data-toggle="tooltip" title="Payment"></i></i></a>
+                <a href="editServ" class="edit" onclick="editForm({{$service->id}})" data-toggle="modal"><i class="fa fa-money" data-toggle="tooltip" title="Payment"></i></i></a>
                 <a href="{{url('invoice/generate-pdf')}}" class="export"><i class="material-icons" data-toggle="tooltip" title="View">print</i></a>
-                <a href="deleteInvoice" class="delete" onclick="deleteInvoice({{$service->id}})" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+                <a href="deleteServ" class="delete" onclick="deleteServ({{$service->id}})" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
 
                </td>
              </tr>
@@ -182,135 +171,7 @@
   <!-- content-wrapper ends -->
 </div>
 
-<!-- Edit Modal HTML -->
- <div id="invoiceForm" class="modal fade">
-                <div class="modal-dialog">
-                  <div class="modal-content">
-                    <div class="form-body">
-                      <div class="row">
-                        <div class="col-12 grid-margin stretch-card">
-                          <div class="card">
-                            <div class="card-body">
-                              <h4 class="card-title">Invoice Form</h4>
-                              <p class="card-description">Please fill in your client's invoice details.</p>
-                                      @if(Session::get('success'))
-                                      <div class="alert alert-success">
-                                        {{Session:get('success')}}
-                                      </div>
-                                      @endif
 
-                                      @if(Session::get('fail'))
-                                      <div class="alert alert-danger">
-                                        {{Session:get('fail')}}
-                                      </div>
-                                      @endif
-
-                                      <form action="{{route("updateInvoice")}}" method="POST" class="forms-sample">
-                                        @csrf
-                                        <input type="hidden" name="id" id="invoiceid" value="">
-                                          <div class="form-group">
-                                            <label for="InputName1">Fullname</label>
-                                            <input class="form-control" type="text" name="name" id="name" value="" placeholder="Name" disabled>
-                                          </div>
-
-                                          <div class="form-group">
-                                            <label for="InputPhone">Phone Number</label>
-                                            <input class="form-control" type="text" name="phone" id="phone" value="" pattern="^(\+?6?01)[02-46-9]-*[0-9]{7}$|^(\+?6?01)[1]-*[0-9]{8}$" placeholder="0112366789" disabled>
-                                        </div>
-
-                                          <div class="form-group">
-                                              <label for="InputEmail">Email address</label>
-                                              <input class="form-control" type="email" name="email" id="email" value=""  placeholder="Email" disabled>
-                                          </div>
-
-                                          <div class="form-group">
-                                            <label for="InputCompany">Company</label>
-                                            <input class="form-control" type="text" name="company" id="company" value=""  placeholder="Company name" disabled>
-                                        </div>
-                                        <div class="form-group">
-                                        <div class="input-group">
-                                          <div class="input-group-prepend">
-                                            <span class="input-group-text bg-primary text-white">RM</span>
-                                          </div>
-                                          <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
-                                        
-                                        </div>
-                                      </div>
-                                      <div class="form-group">
-                                      <div class="input-group">
-                                        <div class="input-group-prepend">
-                                          <button class="btn btn-sm btn-outline-primary dropdown-toggle" type="button" data-toggle="dropdown">Payment Status</button>
-                                          <div class="dropdown-menu" x-placement="bottom-start" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(0px, 46px, 0px);">
-                                            <a class="dropdown-item" href="#" name="paid" id="paid" value="Paid">Paid</a>
-                                            <a class="dropdown-item" href="#" name="unpaid" id="unpaid" value="Unpdaid">Unpaid</a>
-                                          </div>
-                                        </div>
-                                      </div>
-                                    </div>
-
-                                        {{-- Services:
-                                        <div class="form-check">
-                                          <label class="form-check-label">
-                                            <input class="form-check-input" type="checkbox" name="services[]" id="ups" value="UPS"  id="invalidCheck">Uninterruptible Power Supply Precision Cooling Solution</label>
-                                      </div>
-                                      <div class="form-check">
-                                        <label class="form-check-label">
-                                          <input class="form-check-input" type="checkbox" name="services[]" id="fm" value="Facility Management"  id="invalidCheck">Facility Management M&E Maintenance</label>
-                                    </div>
-                                    <div class="form-check">
-                                      <label class="form-check-label">
-                                        <input class="form-check-input" type="checkbox" name="services[]" id="dce" value="Data Center Enhancement"  id="invalidCheck">Data Center Enhancement ICT Managed Services</label>
-                                  </div>
-                                  <div class="form-check">
-                                    <label class="form-check-label">
-                                      <input class="form-check-input" type="checkbox" name="services[]" id="cs" value="Corporate Support"  id="invalidCheck">Corporate Support Business Advisory</label>
-                                </div><br> --}}
-                                
-                                {{-- Select Date & Time:
-                                <div class="form-group">
-                                  <input class="" type="date" name="date" id="date" value="" >
-                                  <input class="" type="time" name="time" id="time" value="" >
-                                  </div> --}}
-
-                                      {{-- <div class="col-md-12">
-                                          <textarea class="form-control" type="text" name="issue" id="issue" value="" placeholder="State your issue" required></textarea>
-                                          <div class="invalid-feedback">Issue field cannot be blank!</div>
-                                      </div> --}}
-                                                <button type="button" class="btn btn-light" onclick="closeModal()">Cancel</button>
-                                                <input type="submit" class="btn btn-primary mr-2" value="Save">
-                                      </form>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-                  </div>
-                </div>
-              </div>
-
-  <!-- Delete Client HTML -->
-  <div id="deleteInvoice" class="modal fade">
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <form action="{{route("deleteInvoice")}}" method="POST">
-                    @csrf
-                    <input type="hidden" name="id" id="delId" value="">
-                    <div class="modal-header">						
-                      <h4 class="modal-title">Delete Service Record</h4>
-                      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                    </div>
-                    <div class="modal-body">					
-                      <p>Are you sure you want to delete the Services Records?</p>
-                      <p class="text-danger"><small>This action cannot be undone.</small></p>
-                    </div>
-                    <div class="modal-footer">
-                      <button type="button" class="btn btn-default" onclick="closeModalDelete()">Cancel</button>
-                      <input type="submit" class="btn btn-danger" value="Proceed" >
-                    </div>
-                  </form>
-                </div>
-              </div>
-            </div>
 
 
 
@@ -349,75 +210,6 @@
     <script src="index.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.16/dist/sweetalert2.all.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-  
-        <script>
-      function editForm(id){
-
-        $("#id").val("");
-        $("#name").val("");
-        $("#phone").val("");
-        $("#email").val("");
-        $("#company").val("");
-
-
-
-        $.ajax({
-            type: "GET",
-            url: '{{url("invoice/edit/")}}'+'/'+id,
-            dataType: "json",
-            success: function (data) {
-                // console.log(data);
-                $("#pendingid").val(data["id"]);
-                $("#name").val(data["name"]);
-                $("#phone").val(data["phone"]);
-                $("#email").val(data["email"]);
-                $("#company").val(data["company"]);
-                $("#amount").val(data["amount"]);
-                $("#paymentStatus").val(data["paymentStatus"]);
-
-                var services = data["services"].split(',');
-                for (let i = 0; i < services.length; i++) {
-
-                  var serv = services[i].trim();
-
-                  if(serv == "UPS"){
-                    $( "#ups" ).prop( "checked", true );
-                  }else if(serv == "Facility Management"){
-                    $( "#fm" ).prop( "checked", true );
-                  }else if(serv == "Data Center Enhancement"){
-                    $( "#dce" ).prop( "checked", true );
-                  }else if(serv == "Corporate Support"){
-                    $( "#cs" ).prop( "checked", true );
-                  }
-
-                }
-
-
-                $('#invoiceForm').modal('show');
-            },
-            error: function (data) {
-                console.log('Error:', data);
-
-            }
-        });
-
-      }
-
-      function closeModal(){
-        $('#invoiceForm').modal('hide');
-      }
-
-        function deleteInvoice(id){
-        $("#delId").val(id);
-        $('#deleteInvoice').modal('show');
-      }
-
-      function closeModalDelete(){
-        $('#deleteInvoice').modal('hide');
-      }
-
-      </script>
-
   </body>
 </html>
 
